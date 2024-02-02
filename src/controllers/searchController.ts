5aec00a4c42b3762f2c60b39af299d9fe5aa3b16
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2024-02-02 10:40:29 -0300

    Validação de Search

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -11,6 +11,11 @@ import { createMenuObject } from '../helpers/createMenuObject';
 export const search = (req: Request, res: Response) => {
     let query: string = req.query.q as string;
 
+    if(!query) {
+        res.redirect('/');
+        return;
+    }
+
     let list = Pet.getFromName(query);
     res.render('pages/page', {
         menu: createMenuObject(''),
